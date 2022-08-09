b83b3609a86ee9d541a49d01e658ce7b519f65c2
shri hekale <shrihekale@gmail.com>
2022-08-10 00:49:08 +0530

    first commit

---
+++ Demo1.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Demo1
+{
+    
+ /*   public static class superclass
+    {
+        static void print()
+        {
+            System.out.println("superclass.");
+        }
+    }
+    public static class subclass extends superclass
+    {
+        static void print()
+        {
+            System.out.println("subclass.");
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        superclass A = new superclass();
+        superclass B = new subclass();
+        A.print();
+        B.print();
+    }*/
+    
+   
+   // public static void main(String[] args) {
+  //      int a = 5 * 1 + 3 / 4 - 7;
+  //      int b = (2 * 4) + (4 / 3) - 12;
+  //      System.out.print(a + ", " + b);
+   // }
+   
+   
+    public static void main(String []args){
+        int number = 10;
+       String result = number%2==0 ? "Odd" : "Even";
+       
+       System.out.println(result);
+       
+         System.out.println("" + 7 + 6); 
+}
+
+}
+
+
